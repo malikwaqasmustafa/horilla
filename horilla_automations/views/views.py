1d529918fe563a94212e1ee5bb716350080e2fe0
Waqas Mustafa <malik.waqas.09@hotmail.com>
2025-04-13 19:49:24 +0500

    changes v2
    changes v2

--- horilla_automations/views/views.py
+++ horilla_automations/views/views.py
@@ -12,8 +12,6 @@ from horilla.decorators import login_required, permission_required
 from horilla_automations.methods.methods import generate_choices
 from horilla_automations.methods.serialize import serialize_form
 from horilla_automations.models import MailAutomation
-from horilla_automations.signals import REFRESH_METHODS
-from horilla_views.generic.cbv.views import HorillaFormView
 
 
 @login_required
@@ -45,7 +43,7 @@ def get_to_field(request):
 
 
 @login_required
-@permission_required("horilla_automations.delete_mailautomation")
+@permission_required("automations")
 def delete_automation(request, pk):
     """
     Automation delete view
@@ -57,15 +55,3 @@ def delete_automation(request, pk):
         print(e)
         messages.error(request, "Something went wrong")
     return redirect(reverse("mail-automations"))
-
-
-@login_required
-@permission_required("horilla_automations.add_mailautomation")
-def refresh_automations(request):
-    """
-    Method to  refresh automation signals
-    """
-    REFRESH_METHODS["clear_connection"]()
-    REFRESH_METHODS["start_connection"]()
-    messages.success(request, "Automations refreshed")
-    return HorillaFormView.HttpResponse()
